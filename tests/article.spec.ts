907ece95155713e6f76737fc2dc309d7e312820f
NA1502 <akshat.narang@nl.abnamro.com>
2023-03-09 17:23:34 +0100

    added deletion article as setup

--- tests/article.spec.ts
+++ tests/article.spec.ts
@@ -1,5 +1,8 @@
-import { test, expect } from '../fixtures/pageFixtures';
+import { test } from '../fixtures/pageFixtures';
+import { ArticleApi } from '../rest/ArticleApi';
 
 test('should be able to create article', async({ articleEditorPage }) => {
+    const articleApi = new ArticleApi();
+    await articleApi.deleteArticleByTitle('Creating new article');
     await articleEditorPage.createArticle();
 });
